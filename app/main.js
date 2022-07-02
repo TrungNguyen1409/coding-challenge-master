c8e6e786495b1a8234c9eb013786fcdbfcba3781
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-07-02 15:31:27 +0200

    mess everything up

---
+++ app/main.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  // 👇️ Change text color globally
+  document.body.style.color = 'darkgreen';
+
+  // 👇️ Change text color for clicked element only
+  // event.target.style.color = 'salmon';
+});
--- node-backend/server.js
+++ node-backend/server.js
@@ -1,29 +1,36 @@
 // jshint esversion: 6
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { uuid } = require('uuidv4');
-
+const bodyParser = require("body-parser");
 
 const fs = require('fs');
+const https = require('https');
 const http = require('http');
+
 
 const nodestatic = require('node-static');
 const randopeep = require('randopeep'); // using to generate new random, fake data
 
-
 const app = express();
-var file = new nodestatic.Server('../app');
+const appStatic = express();
+//var file = new nodestatic.Server('../app');
 
+appStatic.use('/static', express.static(path.join(__dirname, 'app')));
+const server = http.createServer(appStatic);
+const PORT = 8080 || process.env.PORT;
+server.listen(PORT, () => console.log(`StaticServer running on port ${PORT}`));
+
+appStatic.get("/",function(req,res){
+
+    res.sendFile("/Users/trungnguyen/Desktop/coding-challenge-master/app/index.html");
+   
+   });
 
 // enable CORS to allow requests from frontend
 app.use(cors()); 
-
-// register handler to return driver data
-app.get('/', function (req, res) {
-    fs.readFile('./index.get.json', 'utf8' , (err, data) => {
-        res.send(data);
-    });
-});
+appStatic.use(bodyParser.urlencoded({extended: true}));
 
 
 fs.writeFileSync("./index.get.json", '[]');
@@ -50,19 +57,60 @@ function getRenterData() {
 }
 
 //TODO: Move driver to different location randomly every 5 seconds
+function moveDriverLocation(){
 
+}
 
-// Create the server for serving static files (html, css etc.)
-http.createServer(function (request, response) {
+/* // Create the server for serving static files (html, css etc.)
+  http.createServer(function (request, response) {
     request.addListener('end', function () {
         //
         // Serve files!
         //
         file.serve(request, response);
     }).resume();
-}).listen(8080);
+}).listen(8080);  */
 
+
+// register handler to return driver data
+app.get('/', function (req, res) {
+    fs.readFile('./index.get.json', 'utf8' , (err, data) => {
+        res.send(data);
+    });
+});
 // Start the REST API server
 app.listen(3000, function() {
     console.log(`API Server is running`)
 });
+
+
+
+
+
+
+
+appStatic.post("/",function(req,res){
+    console.log(req.body.cityName);
+    console.log("Post request received");
+  
+    const apiKey = "642b59f7e25207e8d23c497d5f7ad128";
+    const city = req.body.cityName;
+    const units = "metric";
+    const url ="https://api.openweathermap.org/data/2.5/weather?appid="+ apiKey +"&q="+city+"&units=" +units;
+  
+    https.get(url,function(response){
+      response.on("data",function(data){
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const city = weatherData.name;
+        const icon = weatherData.weather[0].icon;
+        const iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write("<h1> The Temperatur in " +city+  " is: " + temp + " Celcius degree</h1>");
+        res.write("<p> How it is felt like: " + description + "</p>");
+        res.write("<img src=" + iconURL + ">");
+  
+        res.send();
+      });
+    });
+  });
